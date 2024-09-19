45e522f6d57867fadf33476615d4f0fff60d0a10
afroz210 <afroznisa8252@gmail.com>
2024-09-19 13:08:56 +0600

    update balance in DOM

--- JS/home.js
+++ JS/home.js
@@ -12,4 +12,26 @@ document.getElementById('button-add-money').addEventListener('click' , function(
     //get the pin number provided
     const inputPassword = document.getElementById('input-password').value;
     console.log(inputPassword)
+
+    // step 3 - verify pin number
+    if(inputPassword === '1234'){
+        console.log('adding money to your account')
+
+        //step 4 - get the current value
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+
+        //step 5 - add inputAmount with balance
+        const numberOfAmount = parseFloat(inputAmount);
+        const numberOfBalance = parseFloat(balance)
+        const newBalance = numberOfAmount + numberOfBalance
+        console.log(newBalance);
+
+        //step 6 - update the balance in UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+
+    }else{
+        alert('Failed to add money! Please try again')
+    }
 })
